bcada60876939e94267b836e9286e94622c48023
me2two <kempemills@gmail.com>
2019-10-09 21:20:23 -0700

    Made changes to AddingMachine and Added JUnit Test File.

--- src/cse360assign2/AddingMachine.java
+++ src/cse360assign2/AddingMachine.java
@@ -3,31 +3,41 @@ package cse360assign2;
 public class AddingMachine {
 	
 	private int total;
+	private String history;
 	
 	public AddingMachine () {
 		total = 0;
+		history = "0";
 		// not needed - included for clarity
 	}
 	
 	public int getTotal () {
-			return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-				
+		total += value;
+		history += " + " + value;
+		System.out.println(total);
+		System.out.println(history);
 	}
 			
 	public void subtract (int value) {
-		
+		total -= value;
+		history += " - " + value;
+		System.out.println(total);
+		System.out.println(history);
 	}
 	
 	public String toString () {
-		return "";
+		return history;
 	}
 	
 	public void clear() {
-			
+			total = 0;
+			history = "0";
+			System.out.println(total);
+			System.out.println(history);
 	}
-		
 	
 }
---
+++ src/cse360assign2/AddingMachineTest.java
@@ -0,0 +1,28 @@
+package cse360assign2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddingMachineTest {
+
+	@Test
+	public void test() {
+		
+		AddingMachine mac = new AddingMachine();
+		mac.add(5);
+		mac.add(7);
+		mac.subtract(19);
+		mac.add(4);
+		mac.subtract(1);
+		mac.add(23);
+		
+		assertTrue(mac.getTotal() == 19);
+		assertTrue(mac.toString().equals("0 + 5 + 7 - 19 + 4 - 1 + 23"));
+		mac.clear();
+		mac.add(3);
+		assertTrue(mac.getTotal() == 3);
+		assertTrue(mac.toString().equals("0 + 3"));
+	}
+
+}
